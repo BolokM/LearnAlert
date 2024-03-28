667d1683186cbdcf4a5b544cb57980254348c133
Blake Miller <bam55970@gmail.com>
2024-03-27 21:49:55 -0400

    Initial Commit

---
+++ LearnAlert/LearnAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnAlertApp.swift
+//  LearnAlert
+//
+//  Created by Blake Miller on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
